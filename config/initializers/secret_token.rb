5141262dea9b2a4a39f2fce1380cf2e48454f770
Brian Han <brianhan87usa@gmail.com>
2013-12-05 01:37:49 -0500

    first commit -- gemfile updated and new controller for home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brianhansite::Application.config.secret_key_base = 'c760a9c739b9d657de05f28e7709e680a494b83b7df7264adc874cddef4ac0a2518f1f7bbac95ca89ff398d0c28703bad75e2cd7ba82fedf29e309670c3c5d3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brianhansite::Application.config.session_store :cookie_store, key: '_brianhansite_session'
